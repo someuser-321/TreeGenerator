146acaab7330f616736ff5d8495ec001acf69366
Chris Harrison <someuser-321@users.noreply.github.com>
2017-01-14 20:22:59 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ function setup()
 	slider_y.position(10, 10);
 	slider_level = createSlider(1, 13, 11, 1);
 	slider_level.position(10, 30);
-	slider_rot = createSlider(0, PI/2, (PI/2)/4, (PI/2)/(3*4*8*10));
+	slider_rot = createSlider(0, PI/2, (PI/2) / 4, (PI/2) / (3 * 5 * 8));
 	slider_rot.position(10, 50);
 	slider_lenRand = createSlider(0, 1, 1, 0.01);
 	slider_lenRand.position(10, 70);
@@ -97,7 +97,7 @@ function setup()
 	button_newSeed = createButton("Generate new tree");
 	button_newSeed.position(10, 190);
 	button_newSeed.mousePressed(function(){
-		randSeed = Math.floor(Math.random()*1000);
+		randSeed = Math.floor(Math.random() * 1000);
 		prog = 100;
 		input_seed.value(randSeed);
 		startGrow();
@@ -108,14 +108,14 @@ function setup()
 	button_randomParams.mousePressed(function(){
 		randomSeed(paramSeed);
 		
-		slider_level.value(1*slider_level.value() + 0.1*(0.5-rand())*(slider_level.attribute('max') - slider_level.attribute('min')));
-		slider_rot.value(1*slider_rot.value() + 0.1*(0.5-rand())*(slider_rot.attribute('max') - slider_rot.attribute('min')));
-		slider_lenRand.value(1*slider_lenRand.value() + 0.1*(0.5-rand())*(slider_lenRand.attribute('max') - slider_lenRand.attribute('min')));
-		slider_branchProb.value(1*slider_branchProb.value() + 0.1*(0.5-rand())*(slider_branchProb.attribute('max') - slider_branchProb.attribute('min')));
-		slider_rotRand.value(1*slider_rotRand.value() + 0.1*(0.5-rand())*(slider_rotRand.attribute('max') - slider_rotRand.attribute('min')));
-		slider_leafProb.value(1*slider_leafProb.value() + 0.1*(0.5-rand())*(slider_leafProb.attribute('max') - slider_leafProb.attribute('min')));
+		slider_level.value(1 * slider_level.value() + 0.1 * rand2() * (slider_level.attribute('max') - slider_level.attribute('min')));
+		slider_rot.value(1 * slider_rot.value() + 0.1 * rand2() * (slider_rot.attribute('max') - slider_rot.attribute('min')));
+		slider_lenRand.value(1 * slider_lenRand.value() + 0.1 * rand2() * (slider_lenRand.attribute('max') - slider_lenRand.attribute('min')));
+		slider_branchProb.value(1 * slider_branchProb.value() + 0.1 * rand2() * (slider_branchProb.attribute('max') - slider_branchProb.attribute('min')));
+		slider_rotRand.value(1 * slider_rotRand.value() + 0.1 * rand2() * (slider_rotRand.attribute('max') - slider_rotRand.attribute('min')));
+		slider_leafProb.value(1 * slider_leafProb.value() + 0.1 * rand2() * (slider_leafProb.attribute('max') - slider_leafProb.attribute('min')));
 		
-		paramSeed = 1000*random();
+		paramSeed = 1000 * random();
 		randomSeed(randSeed);
 		
 		readInputs(true);
@@ -164,7 +164,7 @@ function draw()
 	stroke(255, 255, 255);
 	
 	background(0, 0, 0);
-	translate(width/2, height);
+	translate(width / 2, height);
 	scale(1, -1);
 	
 	translate(0, 20);
@@ -172,7 +172,7 @@ function draw()
 	branch(1, randSeed);
 	
 	var endTime = millis();
-	label_perf.html('Generated in ' + Math.floor((endTime-startTime)*10)/10 + 'ms');
+	label_perf.html('Generated in ' + Math.floor((endTime-startTime) * 10) / 10 + 'ms');
 	
 	noLoop();
 }
@@ -187,14 +187,14 @@ function branch(level, seed)
 	var seed1 = random(1000),
 		seed2 = random(1000);
 		
-	var growthLevel = (prog - level > 1) || (prog >= maxLevel+1) ? 1 : (prog - level);
+	var growthLevel = (prog - level > 1) || (prog >= maxLevel + 1) ? 1 : (prog - level);
 	
-	strokeWeight(12 * Math.pow((maxLevel-level+1)/maxLevel, 2));
+	strokeWeight(12 * Math.pow((maxLevel - level + 1) / maxLevel, 2));
 
-	var len = growthLevel * y * (1 + (2*(rand()-0.5) * lenRand));
+	var len = growthLevel * y * (1 + rand2() * lenRand);
 	
-	line(0, 0, 0, len/level);
-	translate(0, len/level);
+	line(0, 0, 0, len / level);
+	translate(0, len / level);
 	
 	
 	var doBranch1 = rand() < branchProb;
@@ -205,21 +205,21 @@ function branch(level, seed)
 	if ( level < maxLevel )
 	{
 		
-		var r1 = rot * (1 + 2*(rand()-0.5) * rotRand);
-		var r2 = -rot * (1 + 2*(rand()-0.5) * rotRand);
+		var r1 = rot * (1 + rand2() * rotRand);
+		var r2 = -rot * (1 + rand2() * rotRand);
 		
 		if ( doBranch1 )
 		{
 			push();
 			rotate(r1);
-			branch(level+1, seed1);
+			branch(level + 1, seed1);
 			pop();
 		}
 		if ( doBranch2 )
 		{
 			push();
 			rotate(r2);
-			branch(level+1, seed2);
+			branch(level + 1, seed2);
 			pop();
 		}
 	}
@@ -228,16 +228,16 @@ function branch(level, seed)
 	{
 		var p = Math.min(1, Math.max(0, prog - level));
 		
-		var flowerSize = y/100 * p * (1/6) * (len/level);
+		var flowerSize = (y / 100) * p * (1 / 6) * (len / level);
 
 		strokeWeight(1);
-		stroke(240 + 15*rand2(), 140 + 15*rand2(), 140 + 15*rand2());
+		stroke(240 + 15 * rand2(), 140 + 15 * rand2(), 140 + 15 * rand2());
 		
 		rotate(-PI);
 		for ( var i=0 ; i<=8 ; i++ )
 		{
-			line(0, 0, 0, flowerSize);
-			rotate(2*PI/8);
+			line(0, 0, 0, flowerSize * (1 + 0.5 * rand2()));
+			rotate(2 * PI/8);
 		}
 	}	
 }
@@ -264,16 +264,16 @@ function grow()
 	var diff = millis() - startTime;
 
 	prog += maxLevel / 8 * Math.max(diff, 20) / 1000;
-	setTimeout(grow, Math.max(1, 20-diff));
+	setTimeout(grow, Math.max(1, 20 - diff));
 }
 
 
 function rand()
 {
-	return random(1000)/1000;
+	return random(1000) / 1000;
 }
 
 function rand2()
 {
-	return random(2000)/1000 - 1;
+	return random(2000) / 1000 - 1;
 }
